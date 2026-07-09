7e0e5c2799f5c112e3dae6bcc2554e5de85ed47b
Pavadharini-G <pavadharini07@gmail.com>
2026-07-09 03:41:28 +0000

    Sync LeetCode submission Runtime - 10 ms (35.92%), Memory - 47.7 MB (25.22%)

--- leetcode-solutions/1019-squares-of-a-sorted-array/solution.java
+++ leetcode-solutions/1019-squares-of-a-sorted-array/solution.java
@@ -1,10 +1,11 @@
+import java.util.*;
 class Solution {
     public int[] sortedSquares(int[] nums) {
         for(int i=0;i<nums.length;i++)
         {
             nums[i]=nums[i]*nums[i];
         }
-        for(int i=0;i<nums.length;i++)
+        /*for(int i=0;i<nums.length;i++)
         {
             for(int j=0;j<nums.length;j++)
             {
@@ -18,7 +19,8 @@ class Solution {
                     }
                 }
             }
-        }
+        }*/
+        Arrays.sort(nums);
         return nums;
     }
 }
