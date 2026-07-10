1f8b4680a88ed322ca56d87ca879a175f1f21a6f
Pavadharini-G <pavadharini07@gmail.com>
2026-07-10 08:18:34 +0000

    Sync LeetCode submission Runtime - 15 ms (59.72%), Memory - 51.4 MB (75.05%)

---
+++ leetcode-solutions/0455-assign-cookies/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=g.length-1;
+        int j=s.length-1;
+        int max=0;
+        while(i>=0 && j>=0)
+        {
+            if(s[j]>=g[i])
+            {
+                max++;
+                i--;
+                j--;
+            }
+            else
+            {
+                i--;
+            }
+        }
+        return max;
+    }
+}
