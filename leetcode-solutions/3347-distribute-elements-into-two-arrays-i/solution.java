ec5e5cbf4edae04b331016b8995b80a42a9b2d04
Pavadharini-G <pavadharini07@gmail.com>
2026-08-20 04:46:25 +0000

    Sync LeetCode submission Runtime - 2 ms (57.45%), Memory - 46.6 MB (81.25%)

---
+++ leetcode-solutions/3347-distribute-elements-into-two-arrays-i/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n=nums.length;
+        List<Integer> arr1=new ArrayList<>();
+        List<Integer> arr2=new ArrayList<>();
+        arr1.add(nums[0]);
+        arr2.add(nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            int a=arr1.size()-1;
+            int b=arr2.size()-1;
+            if(arr1.get(a)>arr2.get(b))
+            {
+                arr1.add(nums[i]);
+            }
+            else
+            arr2.add(nums[i]);
+        }
+        arr1.addAll(arr2);
+        int[] result=new int[arr1.size()];
+        for(int i=0;i<arr1.size();i++)
+        {
+            result[i]=arr1.get(i);
+        }
+        return result;
+    }
+}
